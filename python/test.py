af657dea72b4501c707fa6cfb3b35a7f7d971d99
Srinivas <srinivas9804@gmail.com>
2019-06-10 10:07:52 +0100

    added timestamp

--- python/test.py
+++ python/test.py
@@ -1,9 +1,15 @@
 import pymongo
 import serial 
+import datetime
 
-myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-mydb = myclient["mydb"]
-mycol = mydb["Sensor"]
+dt = datetime.datetime.now()
+ts = dt.timestamp()#make sure to divide timestamp by 1000 to obtain
+# JS timestamp
+print(ts," ", dt)
 
-mydict = { "name": "John", "address": "Highway 37" }
-x = mycol.insert_one(mydict)
+#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+#mydb = myclient["mydb"]
+#mycol = mydb["Sensor"]
+#
+#mydict = { "name": "John", "address": "Highway 37" }
+#x = mycol.insert_one(mydict)
