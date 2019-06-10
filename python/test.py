4221eb151a781e71450eda83f54f49d37adcc0dd
srinivas <srinivas9804@gmail.com>
2019-06-10 11:58:52 +0100

    added timestamp

--- python/test.py
+++ python/test.py
@@ -7,9 +7,10 @@ ts = dt.timestamp()#make sure to divide timestamp by 1000 to obtain
 # JS timestamp
 print(ts," ", dt)
 
-#myclient = pymongo.MongoClient("mongodb://localhost:27017/")
-#mydb = myclient["mydb"]
-#mycol = mydb["Sensor"]
-#
-#mydict = { "name": "John", "address": "Highway 37" }
-#x = mycol.insert_one(mydict)
+if __name__ == '__main__':
+	myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+	mydb = myclient["mydb"]
+	mycol = mydb["Sensor"]
+
+	mydict = { "temperature": 105, "humidity": 45, "timestamp" : int(ts*1000.0)}
+	x = mycol.insert_one(mydict)
