7cd33e96d540407e7f71d6f328b73288d9cc6eeb
srinivas <srinivas9804@gmail.com>
2019-06-17 17:31:44 +0100

    Added Dynamic charts and changed the design and layout

--- public/sketch.js
+++ public/sketch.js
@@ -5,7 +5,18 @@ setTimeout(() => {
 }, 30000);
 */
 let lat, lon, sensorData;
+let tempChart, moistChart, lightChart, phChart;
+let chartArr;
 
+let dayButton = document.getElementById('dayButton');
+let weekButton = document.getElementById('weekButton');
+let monthButton = document.getElementById('monthButton');
+let yearButton = document.getElementById('yearButton');
+let maxButton = document.getElementById('maxButton');
+let buttonArr = [dayButton,weekButton,monthButton,yearButton,maxButton];
+let indexCtr = 4;
+maxButton.style.color = '#fff';
+maxButton.style.background = '#36A2EB'; 
 
 if ('geolocation' in navigator) {
   console.log('geolocation available');
@@ -15,11 +26,12 @@ if ('geolocation' in navigator) {
     const api_url = `/weather/${lat},${lon}`;
     const response = await fetch(api_url);
     const json = await response.json();
+    const location = document.getElementById('location');
     const temp = document.getElementById('tempValue');
     const humid = document.getElementById('humidValue');
     temp.textContent = json.currently.temperature;
     humid.textContent = json.currently.humidity;
-    console.log(json);
+    location.textContent = json.timezone;
   });
 }
 else{
@@ -27,10 +39,46 @@ else{
 }
 getData();
 
+
+
+function onClickListener(button){
+  if(button == dayButton){
+    newCtr = 0;
+  }
+  else if(button == weekButton){
+    newCtr = 1;
+  }
+  else if(button == monthButton){
+    newCtr = 2;
+  }
+  else if(button == yearButton){
+    newCtr = 3;
+  }
+  else{
+    newCtr = 4;
+  }
+
+  if(newCtr != indexCtr){
+    for(var i =0; i < buttonArr.length; i++){
+      if(i == newCtr){
+        buttonArr[i].style.color = '#fff';
+        buttonArr[i].style.background = '#36A2EB';
+      }
+      else{
+        buttonArr[i].style.color = '#36A2EB';
+        buttonArr[i].style.background = '#fff';
+      }
+    }
+    indexCtr = newCtr;
+    updateChart();
+  }
+}
+
+
 async function getData(){
   const response = await fetch('/api');
   sensorData = await response.json();
-  console.log(sensorData.length);
+  console.log('Data length = ' + sensorData.length);
   if(sensorData.length > 0){
     table = document.getElementById('table');
 
@@ -88,21 +136,22 @@ async function chartIt(){
   const moisture = [];
   const ph = [];
   const light =[];
-  const timestamp = [];
 
   for(item of sensorData){
-    temperature.unshift(item.temperature);
-    moisture.unshift(item.moisture);
-    ph.unshift(item.ph);
-    light.unshift(item.light);
-    timestamp.unshift(item.timestamp);
+    var temp_item = {x:item.timestamp, y:item.temperature};
+    var moist_item = {x:item.timestamp, y:item.moisture};
+    var ph_item = {x:item.timestamp, y:item.ph};
+    var light_item = {x:item.timestamp, y:item.light};
+    temperature.unshift(temp_item);
+    moisture.unshift(moist_item);
+    ph.unshift(ph_item);
+    light.unshift(light_item);
   }
 
 
-  var tempChart = new Chart(ctx1, {
+  tempChart = new Chart(ctx1, {
     type: 'line',
     data: {
-        labels: timestamp,
         datasets: [{
             label: 'Temperature',
             data: temperature,
@@ -118,21 +167,15 @@ async function chartIt(){
         scales: {
             xAxes: [{
                 type: 'time',
-                distribution:'series',
-                time: {
-                  displayFormats: {
-                        second: 'h:mm:ss'
-                    }
-                }
+                distribution:'series'
             }]
         }
     }
   });
 
-  var moistChart = new Chart(ctx2, {
+  moistChart = new Chart(ctx2, {
     type: 'line',
     data: {
-        labels: timestamp,
         datasets: [{
             label: 'Moisture',
             data: moisture,
@@ -148,21 +191,15 @@ async function chartIt(){
         scales: {
             xAxes: [{
                 type: 'time',
-                distribution:'series',
-                time: {
-                    displayFormats: {
-                        second: 'h:mm:ss'
-                    }
-                }
+                distribution:'series'
             }]
         }
     }
   });
 
-  var phChart = new Chart(ctx3, {
+  phChart = new Chart(ctx3, {
     type: 'line',
     data: {
-        labels: timestamp,
         datasets: [{
             label: 'pH',
             data: ph,
@@ -178,21 +215,15 @@ async function chartIt(){
         scales: {
             xAxes: [{
                 type: 'time',
-                distribution:'series',
-                time: {
-                    displayFormats: {
-                        second: 'h:mm:ss'
-                    }
-                }
+                distribution:'series'
             }]
         }
     }
   });
 
-  var lightChart = new Chart(ctx4, {
+  lightChart = new Chart(ctx4, {
     type: 'line',
     data: {
-        labels: timestamp,
         datasets: [{
             label: 'Light',
             data: light,
@@ -208,14 +239,56 @@ async function chartIt(){
         scales: {
             xAxes: [{
                 type: 'time',
-                distribution:'series',
-                time: {
-                    displayFormats: {
-                        second: 'h:mm:ss'
-                    }
-                }
+                distribution:'series'
             }]
         }
     }
   });
+  chartArr = [tempChart, moistChart, lightChart, phChart];
+  console.log(chartArr[0]);
+}
+
+function updateChart(){
+  const temperature = [];
+  const moisture = [];
+  const ph = [];
+  const light =[];
+  let refTime = 0;
+  if(indexCtr == 0){
+    refTime = 60000;//at the moment 1 minute, change for 1 day
+  }
+  else if(indexCtr == 1){
+    refTime = 60000*5;//5 minutes change to 1 week
+  }
+  else if(indexCtr == 2){
+    refTime = 60000*10;//10 minutes change to 1 month   
+  }
+  else if(indexCtr == 3){
+    refTime = 60000*20;
+  }
+  else{
+    refTime = -1;
+  }
+  first_item = sensorData[sensorData.length-1];
+    first_time = first_item.timestamp;
+    for(var i = sensorData.length-1; i >= 0;i--){
+    if(first_time - sensorData[i].timestamp > refTime && refTime >= 0)//at the moment 1 minute for testing
+      break;
+    item = sensorData[i];
+    var temp_item = {x:item.timestamp, y:item.temperature};
+    var moist_item = {x:item.timestamp, y:item.moisture};
+    var ph_item = {x:item.timestamp, y:item.ph};
+    var light_item = {x:item.timestamp, y:item.light};
+    temperature.unshift(temp_item);
+    moisture.unshift(moist_item);
+    ph.unshift(ph_item);
+    light.unshift(light_item);
+  }
+
+  data = [temperature, moisture, light, ph];
+
+  for(var i =0;i<chartArr.length;i++){
+    chartArr[i].data.datasets[0].data = data[i];
+    chartArr[i].update();
+  }
 }
